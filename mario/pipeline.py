69d3e34df185e357b8815df3d3e3232942eeb71a
meowoodie <woodielove@163.com>
2017-12-20 15:50:20 -0500

    minor changes

--- mario/pipeline.py
+++ mario/pipeline.py
@@ -2,8 +2,7 @@ from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
 
 sc    = SparkContext("local")
-spark = SparkSession \
-	.builder.appName("FirstPipeline") \
+spark = SparkSession(sc) \
 	.read.text("/home/woodie/sfexpress_rawdata_first2500k.txt")
 
 print(spark.count())
