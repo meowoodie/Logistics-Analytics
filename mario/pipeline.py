f5c724a53dc3946b396aacdeb8d0e89a584236dd
meowoodie <woodielove@163.com>
2017-12-20 15:47:02 -0500

    try to read data into df

--- mario/pipeline.py
+++ mario/pipeline.py
@@ -3,6 +3,10 @@ from pyspark.sql.session import SparkSession
 
 sc    = SparkContext("local")
 spark = SparkSession \
+	.builder.appName("FirstPipeline") \
+	.read.text("/home/woodie/sfexpress_rawdata_first2500k.txt")
+
+print(spark.count())
 
 
 
