c0b02a6c17c4f90ccdc05d770019a260e013f92d
Woody Zhu <meowoodie@users.noreply.github.com>
2018-04-10 13:22:32 -0400

    Add files for d3

---
+++ demo/d3/meta.js
@@ -0,0 +1,6 @@
+var attr_list = [
+  "As of Date", "Fiscal Year", "Region", "Borrower Country", "Borrower Country Code",
+  "Project ID", "Project Name", "Procurement Type", "Procurement Category", "Procurement Method",
+  "Product line", "WB Contract Number", "Major Sector", "Contract Description",
+  "Contract Signing Date", "Supplier", "Supplier Country", "Supplier Country Code",
+  "Total Contract Amount (USD)", "Borrower Contract Reference Number"];
