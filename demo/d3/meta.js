fb39016edc671747bdfdfc53564a1ed969b316eb
meowoodie <woodielove@163.com>
2018-07-18 22:51:23 -0400

    remove sfx code

--- demo/d3/meta.js
+++
@@ -1,6 +0,0 @@
-var attr_list = [
-  "As of Date", "Fiscal Year", "Region", "receive area", "receive area code",
-  "Project ID", "Project Name", "Procurement Type", "Procurement Category", "Procurement Method",
-  "Product line", "WB Contract Number", "Send Industry", "Contract Description",
-  "Send Time", "Supplier", "send area", "Send area code",
-  "Deliver time", "Borrower Contract Reference Number"];
